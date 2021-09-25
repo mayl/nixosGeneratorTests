602ef158ab6b4aac055f37f0cdd269fa1910d2d0
Larry May <larrymayster@gmail.com>
2021-09-25 15:24:06 -0400

    Vagrant box working
    Build devshell and include as system-wide package via
    writeShellScriptBin

--- devShellTest/configuration.nix
+++ devShellTest/configuration.nix
@@ -1,10 +1,13 @@
 { config, lib, pkgs, ... }:
 let
-  devEnv = import devEnv.nix;
+  devEnv = import ./devEnv.nix{};
+  devEnvSh = pkgs.writeShellScriptBin "devEnv" ''
+    PRJ_ROOT=$(pwd) ${devEnv.out}/entrypoint
+    '';
 in
 {
   environment.systemPackages = with pkgs; [ 
     vim
-    devEnv
+    devEnvSh
   ];
 }
