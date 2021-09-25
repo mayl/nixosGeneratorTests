283fba3b47266b8ac41eeeb0dabf5d18f2e3790e
Larry May <larrymayster@gmail.com>
2021-09-25 13:25:42 -0400

    Pin nixpkgs
    set NIX_PATH in the nix-shell so that calling nixos-generators
    automatically has everything pinned.

--- shell.nix
+++ shell.nix
@@ -1,6 +1,7 @@
 { sources ? import nix/sources.nix }:
 let
   pkgs = import sources.nixpkgs {};
+  pkgs_path = sources.nixpkgs.outPath;
   nixos-generators = import sources.nixos-generators;
 in
 with pkgs;
@@ -9,4 +10,7 @@ mkShell {
     vagrant
     nixos-generators
   ];
+  shellHook = ''
+    export NIX_PATH="nixpkgs=${pkgs_path}"
+  '';
 }
